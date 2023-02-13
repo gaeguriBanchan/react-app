177012b65997ccb8a463e5dd288a18a46ca80d49
gaeguriBanchan <rlawowls93@gmail.com>
2023-02-14 00:03:11 +0900

    4.14 Event 처리하기

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Avatar = ({ image, isNew }) => {
   return (
-    <div>
+    <div className='avatar'>
       <img className='photo' src={image} alt='avatar' />
       {isNew && <span className='new'>new</span>}
     </div>
